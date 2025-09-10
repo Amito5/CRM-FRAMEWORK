81684c27c9ffec01f56920b4f0f30230ee48eefc
Amit <infoforamit7@gmail.com>
2025-09-10 11:25:37 +0530

    testNG

---
+++ CRM-FRAMEWORK/src/test/java/testNG/DEMO.java
@@ -0,0 +1,18 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class DEMO {
+	@Test
+	public void case1() {
+		System.out.println("THIS IS CASE 1");
+	}
+	@Test
+	public void case2() {
+		System.out.println("THIS IS CASE 2");
+	}
+	@Test
+	public void case3() {
+		System.out.println("THIS IS CASE 3");
+	}
+}
